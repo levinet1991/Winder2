5a446a3d8efc323e00c3463abae83f3f3f6b4cc5
Levinet Nicolai <levinet.nicolai@gmail.com>
2017-08-17 16:55:06 +0300

    Initializarea proiectului utilizind ArduinoUNO.

---
+++ Winder2/Winder2/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Winder2.cpp
+ *
+ * Created: 8/17/2017 4:50:47 PM
+ * Author : CNTS04
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
